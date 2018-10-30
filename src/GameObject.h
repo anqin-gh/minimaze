85479ae5bc4c234d5bdf945b9f635485f130a727
anqin <anqin@openmailbox.org>
2018-10-30 13:07:00 +0100

    style: rename variables

--- src/GameObject.h
+++ src/GameObject.h
@@ -11,7 +11,7 @@ public:
 	virtual ~GameObject() = default;
 
 	void set_location(int8_t x, int8_t y);
-	void set_next_movement(int8_t nx, int8_t ny);
+	void set_next_movement(int8_t dx, int8_t dy);
 
 	// abstract methods defining the interface
 	virtual void update() 				= 0;
@@ -21,7 +21,7 @@ public:
 
 private:
 	int8_t 	m_x, m_y;			// current location
-	int8_t 	m_dx, m_dy; 		// desired movement
+	int8_t 	m_nx, m_ny; 		// desired movement
 	bool 	m_wants_to_move;	// flag to know whether the object wants to move
 };
 
