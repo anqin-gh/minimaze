2ee88e17f271321aa8f8a437ea6c3fce689f8505
anqin <anqin@openmailbox.org>
2018-11-09 14:54:29 +0100

    fix: renderers STDP & STDP2

--- src/RendSTDP.cpp
+++ src/RendSTDP.cpp
@@ -23,38 +23,38 @@ void RendSTDP::refresh() const {
 
 
 void RendSTDP::draw_player(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_VERDE, STDP_C_NEGRO);
+	STDP::PonCursor(x, y);
 	STDP::sout << "P";
 }
 
 void RendSTDP::draw_enemy_lr(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_AMARILLO, STDP_C_NEGRO);
+	STDP::PonCursor(x, y);
 	STDP::sout << "E";
 }
 
 void RendSTDP::draw_enemy_wander(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_ROJO, STDP_C_NEGRO);
+	STDP::PonCursor(x, y);
 	STDP::sout << "W";
 }
 
 void RendSTDP::draw_wall_normal(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_VERDE, STDP_C_NEGRO);
+	STDP::PonCursor(x, y);
 	STDP::sout << "#";
 }
 
 void RendSTDP::draw_wall_mortal(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_ROJO, STDP_C_NEGRO);
+	STDP::PonCursor(x, y);
 	STDP::sout << "*";
 }
 
 void RendSTDP::draw_goal(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_BLANCO, STDP_C_NEGRO);
+	STDP::PonCursor(x, y);
 	STDP::sout << "@";
 }
 
--- src/RendSTDP2.cpp
+++ src/RendSTDP2.cpp
@@ -23,39 +23,51 @@ void RendSTDP2::refresh() const {
 
 
 void RendSTDP2::draw_player(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_VERDE, STDP_C_NEGRO);
-	STDP::sout << "P";
+	STDP::PonCursor(2*x, 2*y);
+	STDP::sout << "PP";
+	STDP::PonCursor(2*x, 2*y+1);
+	STDP::sout << "PP";
 }
 
 void RendSTDP2::draw_enemy_lr(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_AMARILLO, STDP_C_NEGRO);
-	STDP::sout << "E";
+	STDP::PonCursor(2*x, 2*y);
+	STDP::sout << "EE";
+	STDP::PonCursor(2*x, 2*y+1);
+	STDP::sout << "EE";
 }
 
 void RendSTDP2::draw_enemy_wander(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_ROJO, STDP_C_NEGRO);
-	STDP::sout << "W";
+	STDP::PonCursor(2*x, 2*y);
+	STDP::sout << "WW";
+	STDP::PonCursor(2*x, 2*y+1);
+	STDP::sout << "WW";
 }
 
 void RendSTDP2::draw_wall_normal(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_VERDE, STDP_C_NEGRO);
-	STDP::sout << "#";
+	STDP::PonCursor(2*x, 2*y);
+	STDP::sout << "##";
+	STDP::PonCursor(2*x, 2*y+1);
+	STDP::sout << "##";
 }
 
 void RendSTDP2::draw_wall_mortal(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_ROJO, STDP_C_NEGRO);
-	STDP::sout << "*";
+	STDP::PonCursor(2*x, 2*y);
+	STDP::sout << "**";
+	STDP::PonCursor(2*x, 2*y+1);
+	STDP::sout << "**";
 }
 
 void RendSTDP2::draw_goal(int8_t x, int8_t y) const {
-	STDP::PonCursor(x, y);
 	STDP::CambiaColor(A_BOLD, STDP_C_BLANCO, STDP_C_NEGRO);
-	STDP::sout << "@";
+	STDP::PonCursor(2*x, 2*y);
+	STDP::sout << "@@";
+	STDP::PonCursor(2*x, 2*y+1);
+	STDP::sout << "@@";
 }
 
 } // minimaze
